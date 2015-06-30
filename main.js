ebac32758409561ab807e1621e380b12bdc6b1bf
Joanne Seeberger <joanneseeberger@wireless-nat-inside.uiowa.edu>
2015-06-30 16:59:38 -0500

    Initial commit
    Landing page set up

---
+++ main.js
@@ -0,0 +1,50 @@
+// Remove function
+function buttonClickRemove()
+{
+  $('.dialogue').fadeOut('slow');
+}
+
+function highlight(ev) {
+    $(this).toggleClass('highlighted');  
+}
+
+function increaseFont(ev) {
+    $(this).animate({fontSize: "1.1em"}, 100);
+    $(this).toggleClass('highlighted');  
+}
+
+function decreaseFont(ev) {
+    $(this).animate({fontSize: "1em"}, 100);
+    $(this).toggleClass('highlighted');  
+}
+
+function buttonClick()
+{
+    var a = $('body');
+
+    // Get the text from the rounded-button and assign to a header
+    var buttonName = $(this).html();
+    var headerInfo = $('<div class="dialogue"><h3>' + buttonName + '</h3></div>');
+
+    // Append the info from .dialogue.
+    a.append(  headerInfo.fadeIn('slow') );
+        
+    // Append the info from .dialogue.
+    $('.dialogue').on('click', buttonClickRemove); 
+}
+
+
+
+$(document).on('ready', function(ev)
+{
+    $('.rounded-button').on('click', buttonClick) 
+                        .on('mouseover', increaseFont)
+                        .on('mouseleave', decreaseFont);
+//    $('.rounded-button').on('mouseover mouseleave', highlight);
+});
+
+// Short hand way to write document ready--just pass in the handler function 
+// right into the jQuery object.
+// $(function() { 
+// });
+
