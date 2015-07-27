b200dc782f430579cfb69ee2d88cbe3559e632cc
Joanne Seeberger <jpseeberger@gmail.com>
2015-07-27 02:55:41 -0500

    Display inventory data from json file on home page.

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,59 +1,9 @@
 'use strict';
 
-/*Object sorting function from stackoverflow
-  Usage:
-  objSort(object, 'key') --> sort by key (ascending, case in-sensitive)
-  objSort(object, 'key', true) --> sort by key (ascending, case sensitive)
-  objSort(object, ['key', true]) --> sort by key (descending, case in-sensitive)
-  objSort(object, 'key1', 'key2') --> sort by key1 then key2 (both ascending, case in-sensitive)
-  objSort(object, 'key1', ['price', true]) --> sort by key1 (ascending) then key2 (descending), case in-sensitive)
-*/
-function objSort() {
-  var args = arguments,
-    array = args[0],
-    case_sensitive, keys_length, key, desc, a, b, i;
-
-  if (typeof arguments[arguments.length - 1] === 'boolean') {
-    case_sensitive = arguments[arguments.length - 1];
-    keys_length = arguments.length - 1;
-  } else {
-    case_sensitive = false;
-    keys_length = arguments.length;
-  }
-
-  return array.sort(function (obj1, obj2) {
-    for (i = 1; i < keys_length; i++) {
-      key = args[i];
-      if (typeof key !== 'string') {
-        desc = key[1];
-        key = key[0];
-        a = obj1[args[i][0]];
-        b = obj2[args[i][0]];
-      } else {
-        desc = false;
-        a = obj1[args[i]];
-        b = obj2[args[i]];
-      }
-      if (case_sensitive === false && typeof a === 'string') {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-      }
-      if (! desc) {
-        if (a < b) return -1;
-        if (a > b) return 1;
-      } else {
-        if (a > b) return -1;
-        if (a < b) return 1;
-      }
-    }
-    return 0;
-  });
-} //end of objSort() function
-
 
 $(document).ready(function () {
 	
-  var requestURL = "data/inventory_sm2.json";
+  var requestURL = "data/inventory_sm3.json";
   $.getJSON(requestURL, null, function(data){
     var items = [];
     var fieldWidth = 'two';
@@ -63,7 +13,7 @@ $(document).ready(function () {
       // Sort the elements by classification, etc. before creating
       // the html snippet for insertion into inventory form
       for (var i = 0; i < el.length ; i++) {
-        objSort(el, 'classification', 'category', 'sub-category'); 
+        objSort(el, 'classification', 'category', 'subcategory'); 
       }
       // Loop to create html snippet
       for (var i = 0; i < el.length ; i++) {
@@ -74,7 +24,7 @@ $(document).ready(function () {
             case 'photo':
               fieldWidth = 'one';
               break;
-            case 'sub-category':
+            case 'subcategory':
               fieldWidth = 'three';
               break;
             default:
@@ -89,7 +39,7 @@ $(document).ready(function () {
     });
 
     // Insert the snippet into the form
-	var $desc = $('#existing-inventory');
+	var $desc = $('#existingInventory');
     $desc.append(html_snippet);
 
   });
---
+++ public/js/sort_function.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/*Object sorting function from stackoverflow
+  Usage:
+  objSort(object, 'key') --> sort by key (ascending, case in-sensitive)
+  objSort(object, 'key', true) --> sort by key (ascending, case sensitive)
+  objSort(object, ['key', true]) --> sort by key (descending, case in-sensitive)
+  objSort(object, 'key1', 'key2') --> sort by key1 then key2 (both ascending, case in-sensitive)
+  objSort(object, 'key1', ['price', true]) --> sort by key1 (ascending) then key2 (descending), case in-sensitive)
+*/
+function objSort() {
+  var args = arguments,
+    array = args[0],
+    case_sensitive, keys_length, key, desc, a, b, i;
+
+  if (typeof arguments[arguments.length - 1] === 'boolean') {
+    case_sensitive = arguments[arguments.length - 1];
+    keys_length = arguments.length - 1;
+  } else {
+    case_sensitive = false;
+    keys_length = arguments.length;
+  }
+
+  return array.sort(function (obj1, obj2) {
+    for (i = 1; i < keys_length; i++) {
+      key = args[i];
+      if (typeof key !== 'string') {
+        desc = key[1];
+        key = key[0];
+        a = obj1[args[i][0]];
+        b = obj2[args[i][0]];
+      } else {
+        desc = false;
+        a = obj1[args[i]];
+        b = obj2[args[i]];
+      }
+      if (case_sensitive === false && typeof a === 'string') {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+      }
+      if (! desc) {
+        if (a < b) return -1;
+        if (a > b) return 1;
+      } else {
+        if (a > b) return -1;
+        if (a < b) return 1;
+      }
+    }
+    return 0;
+  });
+} //end of objSort() function
+
