60c592b5aecce5b79b4f0776cf42e318f1390293
Joanne Seeberger <jpseeberger@gmail.com>
2015-08-21 07:29:14 -0500

    Added home page readout of database items to replace json file.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,83 +5,138 @@ var fs = require('fs');
 var path = require('path');
 var db = require('../../db');
 
+var classification_data = [];
+var classes = [];
+var leaf = [];
+var topLevelClasses = [];
 
-module.exports = function (app) {
-  var classification_data = [];
-  var classes = [];
-  var leaf = [];
-  var topLevelClasses = [];
-
-  // Build classifications table
-  function getClasses() 
-//  function getClasses(callback(items, classes)) 
-  {
-    // Select all items where the parent_id is null.
-    var sqlTopLevel = "SELECT * FROM classifications ";
-      sqlTopLevel  += "WHERE parent_id IS NULL ORDER BY name";
-    db.all(sqlTopLevel, function(err, rows) {
-      if (!err)
+/*
+// Async type processing:
+function getTopLevelClasses(callback)
+{
+  // Select all items where the parent_id is null.
+  var sqlTopLevel = "SELECT * FROM classifications ";
+    sqlTopLevel  += "WHERE parent_id IS NULL ORDER BY name";
+  db.all(sqlTopLevel, function(err, rows) {
+    if (!err)
+    {
+      for (i = 0; i < rows.length; i++)
       {
-        for (i = 0; i < rows.length; i++)
-        {
-          topLevelClasses[i] = rows[i].name;
-        }
-        console.log("topLevelClasses: ", topLevelClasses);
+        topLevelClasses[i] = rows[i].name;
       }
-      else 
-      {
-        // on error, send nothing
-        // res.json("err": err);
-        console.log('err: ', err);
-      }
-    });
-    
-  } 
-  
-
-  //Create the classes array of objects
-  fs.readFile('./data/inventory.json', 'utf-8', function(err, data) {
-    if(!err)
-    { 
-      inventory_data = JSON.parse(data);
-      // Make array of classification_data types for menu.
-      for (var i = 0; i < inventory_data.inventory.length ; i++) {
-        var j;
-        if (i == 0) {
-          j = 0;
-          classification_data[0] = inventory_data.inventory[0].classification;
-          j++;
-        } else if (inventory_data.inventory[i].classification != inventory_data.inventory[i-1].classification)
-        {
-          classification_data[j] = inventory_data.inventory[i].classification;
-          j++;
-        }
-      }
-
-      //    res.json(data);
-    } 
+      console.log("topLevelClasses: ", topLevelClasses);
+    }
     else 
     {
       // on error, send nothing
-      //    res.json("err": err);
+      // res.json("err": err);
+      console.log('err: ', err);
     }
-  }); 
+  });
 
-    
+  // Do stuff
+  callback(classes);
+}
 
+function getClasses(callback)
+{
+  // Do stuff
+  callback(classes);
+}
+
+function getItems(callback)
+{
+  // Do stuff
+  callback(items);
+}
+
+// Old procedural style (doesnt work in node!! / async)
+//var classes=getClasses();
+//var items = getItems();
+//res.render();
+*/
+
+// Build classifications table
+function getClasses(callback) 
+{
+  // Select all items where the parent_id is null.
+  var sqlTopLevel = "SELECT * FROM classifications ";
+    sqlTopLevel  += "WHERE parent_id IS NULL ORDER BY name";
+  db.all(sqlTopLevel, function(err, rows) {
+    if (!err)
+    {
+      for (i = 0; i < rows.length; i++)
+      {
+        topLevelClasses[i] = rows[i].name;
+      }
+      console.log("topLevelClasses: ", topLevelClasses);
+    }
+    else 
+    {
+      // on error, send nothing
+      // res.json("err": err);
+      console.log('err: ', err);
+    }
+  });
+
+  var sqlClassifications = "SELECT * FROM classifications";
+  db.all(sqlClassifications, function(err, rows) {
+    if (!err)
+    {
+      classes = rows;
+      var sqlItems = "SELECT classifications.name, * FROM items, classifications ";
+        sqlItems += "WHERE classification_id=classifications.id AND unitsavailable != 0 ";
+//        sqlItems += "ORDER BY items.classification_id";
+      db.all(sqlItems, function(err, rows) {
+        if (!err)
+        {
+          items = rows;
+          console.log("items: ", items);
+
+          // BE SURE TO DO THIS INSIDE OF A db.all() CALLBACK
+          callback(items, classes);
+        }
+        else 
+        {
+          // on error, send nothing
+          // res.json("err": err);
+          console.log('err: ', err);
+        }
+      });
+    }
+    else 
+    {
+      // on error, send nothing
+      // res.json("err": err);
+      console.log('err: ', err);
+    }
+  });
+
+} 
+  
+
+module.exports = function (app) {
   app.get('/', function (req, res) {
-    getClasses();
-//    getClasses(function(items, classes) {
-    
-//      console.log('leaf: ', leaf);
 
-      // Show our homepage
+/*    
+    getClasses(function(classes) {
+      getItems(function(items) {
       res.render('home', {
         title: "Harvest Lane Gardens", 
-//        inventory: items, 
-        inventory: inventory_data.inventory, 
+        inventory: items, 
         classifications: topLevelClasses
       });
-//    });
+      });
+    });
+*/
+    
+    getClasses(function(items, classes) {
+      res.render('home', {
+        title: "Harvest Lane Gardens", 
+        inventory: items, 
+        classifications: topLevelClasses
+      });
+    });
   });
 };
+
