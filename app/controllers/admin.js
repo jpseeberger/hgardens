c62318b08f8b38512bbf8374c3e4f334bf74c39f
Steven Davis <sgdavis@bioneos.com>
2015-08-25 15:14:52 -0500

    Created safer handling of admin user data

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -8,7 +8,30 @@ var db = require('../../db');
 
 module.exports = function (app) {
   var userData;
-  userData = JSON.parse(fs.readFileSync('./data/users.json'));
+
+  // Admin user information from flat JSON file
+  // Ensure data directory exists
+  fs.mkdir('./data', function(e) {
+    try
+    {
+      if (!e || e.code === 'EEXIST')
+      {
+        // Data directory is present
+        // Read in the admin information (if exists, if not: no exception will be thrown)
+        userData = JSON.parse(fs.readFileSync('./data/users.json'));
+      }
+      else
+      {
+      console.log('Here');
+        throw e;
+      }
+    }
+    catch (e)
+    {
+      userData = {};
+      console.log('Problems getting the admin login information [No logins will be possible]:\n', e);
+    }
+  });
 
   app.get('/login', function (req, res) {
     res.render('login', {});
