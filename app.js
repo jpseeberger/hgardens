e9e50b94cc1166aa38efc0d075c5de91ceb41bd6
Steven Davis <sgdavis@bioneos.com>
2015-08-25 15:16:46 -0500

    Startup console log message

--- app.js
+++ app.js
@@ -46,4 +46,4 @@ app.use(express.static('public'));
 server = app.listen(3000);
 
 // Put a friendly message on the terminal
-console.log("Express Server, with static files middleware running at http://127.0.0.1:3000/");
+console.log("HGardens started [ip:0.0.0.0, port:3000, data_dir: './data']");
