db8491fec23ac36b9ee1b7b758463a6167883c6d
Joanne Seeberger <jpseeberger@gmail.com>
2015-09-14 11:37:51 -0500

    Made button more visible.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -44,7 +44,12 @@ var quantity = [
   {id:'12', name:"12"},
   {id:'13', name:"13"},
   {id:'14', name:"14"},
-  {id:'15', name:"15"}
+  {id:'15', name:"15"},
+  {id:'16', name:"16"},
+  {id:'17', name:"17"},
+  {id:'18', name:"18"},
+  {id:'19', name:"19"},
+  {id:'20', name:"20"}
 ];
 
 
